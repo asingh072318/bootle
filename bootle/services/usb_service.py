381c2322baa334cd10ab32143d5af3b908788e58
asingh072318 <ankit.bitmsra@gmail.com>
2023-02-26 19:18:55 -0500

    Cleanup

--- bootle/services/usb_service.py
+++ bootle/services/usb_service.py
@@ -9,25 +9,6 @@ logger = logging.getLogger(__name__)
 
 
 class UsbService:
-    # def get_devices(self):
-    #     devices = []
-    #     if platform.system() == 'Windows':
-    #         for drive in range(1, 27):
-    #             device_path = f'{chr(drive + 64)}:/'
-    #             if os.path.exists(device_path):
-    #                 total, used, available = self.get_device_space(device_path)
-    #                 device_size = {'total': total, 'used': used, 'available': available}
-    #                 devices.append(Device(device_path, device_size, device_path))
-    #     else:
-    #         media_path = '/media' if platform.system() == 'Linux' else '/Volumes'
-    #         for device in os.listdir(media_path):
-    #             device_path = os.path.join(media_path, device)
-    #             if os.path.isdir(device_path):
-    #                 total, used, available = self.get_device_space(device_path)
-    #                 device_size = {'total': self.get_human_readable_size(total), 'used': self.get_human_readable_size(used), 'available': self.get_human_readable_size(available)}
-    #                 devices.append(Device(device_path, device_size, device))
-    #     return devices
-
     def get_devices(self):
         devices = []
         for partition in psutil.disk_partitions():
