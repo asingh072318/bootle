27a83265c631d18aeb80582f2f7a1a7f7e168c91
asingh072318 <ankit.bitmsra@gmail.com>
2023-03-03 23:09:03 -0500

    fix device name

--- bootle/models/device.py
+++ bootle/models/device.py
@@ -2,7 +2,7 @@ class Device:
     def __init__(self, device_path, device_size, device):
         self.path = device_path
         self.size = device_size
-        self.name = device
+        self.device = device
 
     def __str__(self):
-        return f"{self.name} ({self.path}) - Total Size: {self.size['total']}, Available Space: {self.size['available']}"
+        return f"{self.device} ({self.path}) - Total Size: {self.size['total']}, Available Space: {self.size['available']}"
